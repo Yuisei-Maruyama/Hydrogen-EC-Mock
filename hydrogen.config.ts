82bf7f85112749ad56b71b929b4799ac62482367
Yuisei-Maruyama <yuisei.metro@gmail.com>
2023-06-15 16:43:19 +0900

    update: tailwind で壊れている部分を修正する

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
+    // storeDomain: import.meta.env.VITE_PUBLIC_STORE_DOMAIN,
+    // storefrontToken: import.meta.env.VITE_PUBLIC_STOREFRONT_API_TOKEN,
+    // storefrontApiVersion: import.meta.env.VITE_PUBLIC_STOREFRONT_API_VERSION,
     storeDomain: 'hydrogen-preview.myshopify.com',
     storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
     storefrontApiVersion: '2022-07',
